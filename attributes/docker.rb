25811899332217d7ee3310d7ccc9a2a8d12351c9
Bruce Markham <bmarkham@foxguardsolutions.com>
2017-07-25 13:14:11 -0400

    Opens Firewall Ports
    Opens Docker's ports in the firewall so that remote administration and swarm creation is possible. Ports are documented here: https://www.digitalocean.com/community/tutorials/how-to-configure-the-linux-firewall-for-docker-swarm-on-centos-7

--- attributes/docker.rb
+++ attributes/docker.rb
@@ -1,2 +1,4 @@
 default['fgs_docker']['docker-version'] = '17.06.0.ce-1.el7.centos'
 default['fgs_docker']['non-sudo-docker-users'] = []
+default['firewall']['firewalld']['permanent'] = true
+default['firewall']['allow_ssh'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -25,4 +25,37 @@ group 'docker' do
   action :create
   members node['fgs_docker']['non-sudo-docker-users']
 end
+
 include_recipe 'chef-client'
+
+include_recipe 'firewall::default'
+
+firewall_rule 'docker' do
+  port 2376
+  protocol :tcp
+  command :allow
+end
+
+firewall_rule 'docker swarm' do
+  port 2377
+  protocol :tcp
+  command :allow
+end
+
+firewall_rule 'docker network disco tcp' do
+  port 7946
+  protocol :tcp
+  command :allow
+end
+
+firewall_rule 'docker network disco udp' do
+  port 7946
+  protocol :udp
+  command :allow
+end
+
+firewall_rule 'docker container ingress network' do
+  port 4789
+  protocol :udp
+  command :allow
+end
